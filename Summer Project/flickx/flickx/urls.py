5e498c2f555d4c5837a6751250b8d87052a367a5
Jotsna22 <122271671+Jotsna22@users.noreply.github.com>
2023-05-07 16:18:26 +0530

    Update urls.py

--- Summer Project/flickx/flickx/urls.py
+++ Summer Project/flickx/flickx/urls.py
@@ -19,4 +19,5 @@ from django.urls import include,path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('flickxchat/', include("flickxchat.urls")),
+    path('flickxmeet/',include("flickxmeet.urls"))
 ]

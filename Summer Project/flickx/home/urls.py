701cc64f97bc837a7a53317181cc858075fbdb26
Amruthavarshini_telu <120393408+amruthavarshinitelu@users.noreply.github.com>
2023-06-29 10:30:25 +0530

    delete flickx

--- Summer Project/flickx/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("start/", views.start,name='start'),
-    path("",views.homepage, name='homepage'),
-    path("signup/",views.signup,name='signup'),
-    path("signin/",views.signin,name='signin')
-]

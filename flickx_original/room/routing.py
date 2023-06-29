16eaf2270449e71f62b9422b343abfb58a849d9e
Amruthavarshini_telu <120393408+amruthavarshinitelu@users.noreply.github.com>
2023-06-29 10:30:04 +0530

    delete flick original

--- flickx_original/room/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import consumers
-
-websocket_urlpatterns = [
-    path(r'ws/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
-]
--- flickx_original/room/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-from room.views import enter_room
-
-urlpatterns = [
-    path('', views.rooms, name='rooms'),
-    path('enter-room/', enter_room, name='enter-room'),
-]
